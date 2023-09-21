8de12a452b00abd4bc1c191a99f21b5a5a675a04
jannes lambrecht <janneslambrecht@hotmail.com>
2023-09-21 15:36:16 +0200

    update languagebutton

---
+++ dist/languageButton.js
@@ -0,0 +1,35 @@
+let languages = ['NL', 'EN']; // Replace with your desired languages
+let currentLanguageIndex = 0;
+const dutchElements =  document.querySelectorAll('[language="nl"]');
+const englishElements =  document.querySelectorAll('[language="en"]');
+function toggleLanguage() {
+  currentLanguageIndex = (currentLanguageIndex + 1) % languages.length;
+  updateLanguage();
+}
+
+function updateLanguage() {
+  let languageElement = document.getElementsByClassName('language-button')[0];
+  languageElement.innerHTML = '' + languages[currentLanguageIndex];
+  if (languages[currentLanguageIndex] === 'NL') {
+    englishElements.forEach(element => {
+      element.style.display = 'none';
+    });
+    dutchElements.forEach(element => {
+      element.style.display = 'block';
+    });
+  } else {
+    englishElements.forEach(element => {
+      element.style.display = 'block';
+    });
+    dutchElements.forEach(element => {
+      element.style.display = 'none';
+    });
+  }
+  
+  // Here, you can add logic to change the content of your slides based on the selected language.
+  // You may need to assign unique IDs or classes to elements you want to change.
+  // Example: document.getElementById('slide-content').innerHTML = 'Hello, ' + languages[currentLanguageIndex];
+}
+
+// Initialize the button text
+updateLanguage();
